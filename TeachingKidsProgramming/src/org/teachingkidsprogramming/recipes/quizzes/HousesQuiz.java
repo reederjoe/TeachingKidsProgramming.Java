e08bb03637e35d258293c5e1aa699d90b3f096df
Matt Wimer <jiraiu@yahoo.com>
2013-06-19 16:05:32 -0700

    quiz in progress- Daniel/Reeder

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/HousesQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/HousesQuiz.java
@@ -1,5 +1,6 @@
 package org.teachingkidsprogramming.recipes.quizzes;
 
+import org.teachingextensions.logo.Tortoise;
 import org.teachingkidsprogramming.recipes.quizzes.graders.HousesQuizGrader;
 
 public class HousesQuiz extends org.teachingkidsprogramming.recipes.quizzes.graders.HousesQuiz
@@ -7,21 +8,43 @@ public class HousesQuiz extends org.teachingkidsprogramming.recipes.quizzes.grad
   public void question1()
   {
     //  The current length is 7
+    length = 7;
   }
   //
   //      Question2
   //      Create a method called medium
   //       that sets the current length to 21
+  @Override
+  public void medium()
+  {
+    // TODO Auto-generated method stub
+    super.medium();
+    length = 21;
+  }
   //
   //
   //      Question3
   //      Create a method called large
   //       that sets the current length to 63
+  @Override
+  public void large()
+  {
+    // TODO Auto-generated method stub
+    super.large();
+    length = 63;
+  }
   //
   //
   //      Question4
   //      Create a method called moveTheLength
   //       that moves the Tortoise the current length
+  @Override
+  public void moveTheLength()
+  {
+    // TODO Auto-generated method stub
+    super.moveTheLength();
+    Tortoise.move(21);
+  }
   //
   //
   //      Question5
