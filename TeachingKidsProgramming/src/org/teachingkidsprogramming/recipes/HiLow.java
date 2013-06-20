57f8afa8c0799b70c62666fb66e301d9bfce5128
Gavin Dent <gavindent@hotmail.com>
2013-06-20 15:40:17 -0700

    highlow variations-reeder/shivam

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/HiLow.java
@@ -1,34 +1,39 @@
 package org.teachingkidsprogramming.recipes;
 
+import java.util.Random;
+
 import org.teachingextensions.logo.utils.Sounds;
 import org.teachingextensions.windows.MessageBox;
-
-import com.spun.util.NumberUtils;
 
 public class HiLow
 {
   public static void main(String[] args)
   {
-    //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
-    int correctNumber = NumberUtils.getRandomInt(1, 100);
-    for (int i = 1; i <= 8; i++)
+    int correctNumber = new Random(100).nextInt(100);
+    int numberOfGuesses = 8;
+    for (int i = 1; i <= numberOfGuesses; i++)
     {
-      int guess = MessageBox.askForNumericalInput("Guess a number");
+      int chancesLeft = numberOfGuesses + 1 - i;
+      int guess = MessageBox.askForNumericalInput("Guess a number (" + chancesLeft + " left)");
+      if (guess < 1 || 100 < guess)
+      {
+        MessageBox.showMessage("Your number " + guess + " is not between 1 and 100");
+      }
       if (guess == correctNumber)
       {
         Sounds.playBeep();
         MessageBox.showMessage("You Won!");
         System.exit(0);
       }
-      else if (guess > correctNumber)
+      else if (correctNumber < guess)
       {
         MessageBox.showMessage("Too High");
       }
-      else if (guess < correctNumber)
+      else
       {
         MessageBox.showMessage("Too Low");
       }
     }
-    MessageBox.showMessage("YOU HAVE LOST");
+    MessageBox.showMessage("YOU HAVE LOST, the correct answer was " + correctNumber);
   }
 }
